9db3ee29c1ecaa4d37de19797ca6a48f95c3a9b1
Glitch (cute-famous-dust) <none>
2020-04-29 12:14:35 +0000

    ✊😘 Updated with Glitch

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const adapter = new FileSync("db.json");
 const db = low(adapter);
 
 // Set some defaults
-db.defaults({ books: []}).write();
+db.defaults({ books: [], users: []}).write();
 
 app.set("view engine", "pug");
 app.set("views", "./views");
@@ -35,20 +35,55 @@ app.get("/books/update/:id", (req, res) => {
     res.render('update',{
         book: rs
     });
-  })
+});
 
 app.get('/books/delete/:id', (req, res) => {
   var id = req.params.id;
   var rs = db.get('books').remove({id: id}).write();
   res.redirect('/books');
+});
+
+
+app.get('/users', (req, res) => {
+  res.render('users',{
+    users: db.get('users').value()
+  });
+});
+
+app.get('/users/edit/:id', (req,res) => {
+  var id = req.params.id;
+  res.render('edit',{
+    id: id
+  })
 })
+
+app.get('/users/delete/:id', (req, res) => {
+  var id = req.params.id;
+  db.get('users').remove({id: id}).write();
+  res.redirect('/users');
+})
+
+app.post('/users/edit/:id', (req, res) => {
+  var name = req.body.name;
+  var id = req.params.id;
+  db.get('users').find({id: id}).assign({name: name}).write();
+  res.redirect('/users');
+})
+
+app.post('/users/create', (req, res) => {
+  var user = req.body;
+  user.id = shortid.generate();
+  db.get('users').push(user).write();
+  res.redirect('/users');
+})
+
 
 app.post("/books/add", (req, res) => {
   var book = req.body;
   book.id = shortid.generate();
   db.get("books").push(book).write();
   res.redirect('/books');
-})
+});
 
 
 app.post("/books/update/:id", (req, res) => {
@@ -57,7 +92,7 @@ app.post("/books/update/:id", (req, res) => {
     var rs = db.get('books').find({id : id}).assign({title: title}).write();
     res.redirect('/books');
 
-})
+});
   
 
   
